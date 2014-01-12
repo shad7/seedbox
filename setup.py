9dbb352a560bcb50dbc8fc719055e5603554a6f8
shad7 <kenny.shad7@gmail.com>
2014-01-11 20:50:04 -0500

    Changed approach for configuration to simplify code and setup. Included is a generator to create a sample configuration file with help, all available options, their type, default value, and what is required.
    Added version 2 of the CopyTask, UnrarTask, and SyncTask. Supporting storage check to avoid using
    too much storage during the process. Supporting parallel sync to optimize throughput of files back to
    home library.
    
    Additional testing and code coverage performed. Performed PEP8 using flake8. Sphinx documentation generated.

--- setup.py
+++ setup.py
@@ -1,73 +1,7 @@
 #!/usr/bin/env python
 
-import os, re, sys
+import setuptools
 
-try:
-    # make sure setuptools are available
-    from ez_setup import use_setuptools
-    use_setuptools()
-except ImportError:
-    # just ignore it
-    pass
-
-from setuptools import find_packages, setup
-from setuptools.command.test import test as TestCommand
-
-class PyTest(TestCommand):
-    def finalize_options(self):
-        TestCommand.finalize_options(self)
-        self.test_args = []
-        self.test_suite = True
-    def run_tests(self):
-        #import here, cause outside the eggs aren't loaded
-        import pytest
-        errno = pytest.main(self.test_args)
-        sys.exit(errno)
-
-PKG_DIR='src'
-
-# handle dynamically getting the version
-# for our application
-VERFILE = os.path.join(PKG_DIR, 'seedbox', '_version.py')
-verstr = 'unknown'
-try:
-    verstrline = open(VERFILE, 'rt').read()
-except EnvironmentError as err:
-    pass    # file is missing
-else:
-    VSRE = r"^__version__ = ['\"]([^'\"]*)['\"]"
-    mo = re.search(VSRE, verstrline, re.M)
-    if mo:
-        verstr = mo.group(1)
-    else:
-        raise RuntimeError('if {0} exists, it is required to be well-formed'.format(VERFILE))
-
-# generate a list of required components
-required = []
-required.append('SQLObject >= 1.3.2')
-required.append('xworkflows')
-required.append('rarfile >= 2.6')
-required.append('shutilwhich')
-required.append('lockfile')
-required.append('BitTorrent-bencode')
-
-data_files = ['LICENSE', 'README', 'README.md']
-
-setup(
-      name='SeedboxManager',
-      version=verstr,
-      description='Provides framework for performing tasks on a seedbox',
-      author='shad7',
-      author_email='kenny.shad7@gmail.com',
-      url='https://github.com/shad7/seedbox',
-      license='MIT',
-      packages=find_packages(PKG_DIR),
-      package_dir={'': PKG_DIR},
-      data_files=data_files,
-      install_requires=required,
-      entry_points={ 'console_scripts': ['seedmgr = seedbox.manager:main'] },
-      zip_safe=False,
-      tests_require=['pytest'],
-      cmdclass = {'test': PyTest},
-)
-
+setuptools.setup(
+    setup_requires=['pbr'],
+    pbr=True)
--- src/seedbox/_version.py
+++
@@ -1,6 +0,0 @@
-"""
-define the version of our application
-"""
-
-__version__ = "0.1.20"
-
