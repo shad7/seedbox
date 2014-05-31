c33cd65d622423abac456caf8f59aadd88f359b6
shad7 <kenny.shad7@gmail.com>
2014-05-31 13:46:32 -0400

    removed workaround for name of package causing issues with version; solved by update from pbr

--- seedbox/tests/test_version.py
+++ seedbox/tests/test_version.py
@@ -10,3 +10,9 @@ class VersionTest(test.BaseTestCase):
     def test_get_versioninfo(self):
         versioninfo = version.version_info
         self.assertIsNotNone(versioninfo)
+
+    def test_version_str(self):
+        self.assertIsNotNone(version.version_string())
+
+    def test_release_str(self):
+        self.assertIsNotNone(version.release_string())
