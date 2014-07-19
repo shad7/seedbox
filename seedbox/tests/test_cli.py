ace6ed7640f8e8b0997e57079cd445a0b254d935
shad7 <kenny.shad7@gmail.com>
2014-07-19 04:14:14 +0000

    fixed class name

--- seedbox/tests/test_cli.py
+++ seedbox/tests/test_cli.py
@@ -5,17 +5,10 @@ from seedbox import db
 from seedbox.tests import test
 
 
-class fake_options(object):
+class FakeOptions(object):
 
     @staticmethod
     def initialize(args):
-        pass
-
-
-class fake_process(object):
-
-    @staticmethod
-    def start():
         pass
 
 
@@ -23,13 +16,13 @@ class CliTestCase(test.ConfiguredBaseTestCase):
 
     def test_cli(self):
         self.patch(db, '_DBAPI', None)
-        self.patch(cli, 'options', fake_options)
+        self.patch(cli, 'options', FakeOptions)
         cli.main()
         self.assertTrue(True)
 
     def test_cli_lock_timeout(self):
         self.patch(db, '_DBAPI', None)
-        self.patch(cli, 'options', fake_options)
+        self.patch(cli, 'options', FakeOptions)
         with open(os.path.join(self.CONF.config_dir, 'seedmgr.lock'), 'w'):
             cli.main()
 
